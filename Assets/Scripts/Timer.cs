4a5ca55517ffbdbe023bdb6af11a1495e0a5276d
Eduardo Sanchez <esanchezh@unsa.edu.pe>
2020-12-03 10:35:56 -0500

    Avance

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+    private DateTime timeNow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeNow = DateTime.Now;
+        Debug.Log(timeNow);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
